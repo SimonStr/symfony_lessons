fad91b7a7c8e883f6939d973fdae265e2cdc30ef
SimonStr <mymail795@gmail.com>
2016-06-21 11:07:48 +0300

    added entity & show them

--- src/AppBundle/Controller/BlogController.php
+++ src/AppBundle/Controller/BlogController.php
@@ -3,16 +3,38 @@
 namespace AppBundle\Controller;
 
 
+use AppBundle\Entity\Post;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * @Route("/blog")
+ */
 
 class BlogController extends Controller
 {
     /**
-     * @Route("/blog", name="show-blog")
+     * @Route("/", name="show-blog")
      */
     public function showBlogAction()
     {
-        return $this->render('blog/show.html.twig');
+        $em = $this->getDoctrine()
+            ->getManager();
+        $posts = $em->getRepository('AppBundle:Post')
+            ->findAll();
+
+        return $this->render('blog/show.html.twig',[
+            'posts' => $posts
+        ]);
+    }
+
+    /**
+     * @Route("/{slug}", name="show-blog-post")
+     */
+    public function showBlogPostAction(Post $post)
+    {
+        return $this->render('blog/show-post.html.twig',[
+            'post' => $post
+        ]);
     }
 }
