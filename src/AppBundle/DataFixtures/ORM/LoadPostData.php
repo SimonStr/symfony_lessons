7cec7a8db440d786855809db5bea9a158fa1363b
SimonStr <mymail795@gmail.com>
2016-06-23 10:48:53 +0300

    added fixtures

---
+++ src/AppBundle/DataFixtures/ORM/LoadPostData.php
@@ -0,0 +1,28 @@
+<?php
+
+
+namespace AppBundle\DataFixtures\ORM;
+
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+
+class LoadPostData implements FixtureInterfaceterface
+{
+    public function load(ObjectManager $manager)
+    {
+        for ($i=0; $i<10; $i++) {
+            $post = new Post();
+            $post->setSlug(sprintf('post_%d',$i));
+            $post->setHeading(sprintf('Post_%d',$i));
+            $post->setContent(<<<HEREDOC
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam neque, pulvinar eget enim viverra, tempor blandit leo. 
+Aenean fringilla libero sit amet leo facilisis, at venenatis dolor cursus. Quisque maximus erat eu arcu pellentesque dignissim. Vestibulum ac egestas sapien. 
+Sed id imperdiet ex. Donec id mi enim. Quisque ullamcorper mi sit amet augue tempor pretium. 
+Phasellus auctor augue a erat aliquam ultricies a sed lectus. Sed vitae sem vitae velit hendrerit imperdiet.
+HEREDOC
+            );
+            $manager->persist();
+        }
+        $manager->flush();
+    }
+}
